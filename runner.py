29e279ca7911cd706f38e56deaad434b1377b670
Davies <ald339@login01.o2.rc.hms.harvard.edu>
2022-06-09 15:44:04 -0400

    Bug fixes
    fix train_pct, use adamW optimizer per paper,  loss only with answer part

--- runner.py
+++ runner.py
@@ -1,27 +1,22 @@
 import torch
-from torch.optim import Adam
+from torch.optim import AdamW
 import torch.nn as nn
 from model import Transformer
-from dataset import (
-    # DEFAULT_DATA_DIR,
-    # EOS_TOKEN,
-    # VALID_OPERATORS,
-    ArithmeticDataset,
-    ArithmeticIterator,
-)
+from dataset import ArithmeticDataset, ArithmeticIterator
 from tqdm import tqdm
 import wandb
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-NUM_EPOCHS = 3e5
+OPTIMIZATION_BUDGET = 3e5
 LOG = True
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+print("Using device:", DEVICE)
 
 if LOG:
     wandb.init(project="grok", settings=wandb.Settings(start_method="thread"))
 
 # get dataloaders
 train_dataset, val_dataset = ArithmeticDataset.splits(
-        train_pct=0.5,
+        train_pct=50, # careful not to use .5
         operator="/",
         operand_length=None,
         data_dir="data",
@@ -47,50 +42,50 @@ model.to(DEVICE)
 model.train()
 
 # get optimizer
-optimizer = Adam(model.parameters(), lr=0.001)
+optimizer = AdamW(model.parameters(), lr=1e-3, weight_decay=1e-5, betas=(0.9, 0.98))
 
 # get criterion
 criterion = nn.CrossEntropyLoss()
 
 # train model
 steps_per_epoch = len(train_dataloader)
-for epoch in tqdm(range(int(NUM_EPOCHS)//steps_per_epoch)):
+for epoch in tqdm(range(int(OPTIMIZATION_BUDGET / steps_per_epoch))):
     # train
     model.train()
-    for batch in train_dataloader:
+    for i, batch in enumerate(train_dataloader):
         optimizer.zero_grad()
-        x, y = batch['text'], batch['target']
-        x = x.to(DEVICE)
+        X, y = batch['text'], batch['target']
+        X = X.to(DEVICE)
         y = y.to(DEVICE)
-        y_hat, _, _ = model(x) # y_hat is 47x6x2000
-        loss = criterion(y_hat.permute(0, 2, 1), y)
+        y_hat, _, _ = model(X) # y_hat is (batch_size, seq_len, vocab_size)
+        # only calculate the loss on the answer part of the equation
+        loss = criterion(y_hat[:, -2, :], y[:, -2])
         loss.backward()
         optimizer.step()
         if LOG:
             wandb.log({
                 "Loss/train": loss.item(), 
                 "epoch": epoch,
-                "Accuracy/train": (y_hat.argmax(dim=2) == y).float().mean().item(),
+                "Accuracy/train": ((y_hat[:, -2, :]).argmax(dim=1) == y[:, -2]).float().mean().item(),
             })
         else:
-            print(f"Epoch {epoch}: train loss {loss.item()}")
+            print(f"Epoch {epoch}: train loss {loss.item()}, train accuracy {((y_hat[:, -2, :]).argmax(dim=1) == y[:, -2]).float().mean().item()}")
     # eval
     model.eval()
     with torch.no_grad():
         for batch in val_dataloader: # only one batch
-            x, y = batch['text'], batch['target']
-            x = x.to(DEVICE)
-            y = y.to(DEVICE)
-            y_hat, _, _ = model(x)
-            loss = criterion(y_hat.permute(0, 2, 1), y)
+            X, y = batch['text'], batch['target']
+            X, y = X.to(DEVICE), y.to(DEVICE)
+            y_hat, _, _ = model(X)
+            loss = criterion(y_hat[:, -2, :], y[:, -2])
             if LOG:
                 wandb.log({
                     "Loss/val": loss.item(), 
                     "epoch": epoch,
-                    "Accuracy/val": (y_hat.argmax(dim=2) == y).float().mean().item(),
+                    "Accuracy/val": ((y_hat[:, -2, :]).argmax(dim=1) == y[:, -2]).float().mean().item(),
                 })
             else:
-                print(f"Epoch {epoch}: test loss {loss.item()}")
+                print(f"Epoch {epoch}: test loss {loss.item()}, test accuracy {((y_hat[:, -2, :]).argmax(dim=1) == y[:, -2]).float().mean().item()}")
 
     # save model
     torch.save(model.state_dict(), "model.pt")
