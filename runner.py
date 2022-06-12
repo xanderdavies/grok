c5735c42c229ee75b31d9d213bf38ac5fb174166
Davies <ald339@login06.o2.rc.hms.harvard.edu>
2022-06-12 19:34:07 -0400

    use nn.Transformer instead of custom

--- runner.py
+++ runner.py
@@ -1,11 +1,12 @@
 import torch
 from torch.optim import AdamW
 import torch.nn as nn
-from model import Transformer
 from dataset import ArithmeticDataset, ArithmeticIterator
 from tqdm import tqdm
 import wandb
 import argparse
+from model import DecoderOnlyTransformer
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description="Replication of grokking behavior observed in Power et al.'s 'Grokking: Generalization Beyond Overfitting on Small Algorithmic Datasets")
 parser.add_argument("--optimization-budget", default=3e5, type=int, help="Number of training steps to run")
@@ -18,7 +19,7 @@ parser.add_argument("--lr", default=1e-3, type=float, help="Learning rate")
 parser.add_argument("--weight-decay", default=1e-5, type=float, help="Weight decay")
 parser.add_argument("--beta1", default=0.9, type=float, help="AdamW beta1")
 parser.add_argument("--beta2", default=0.98, type=float, help="AdamW beta2")
-
+parser.add_argument("--vocab-len", default=2000, type=int, help="Transformer vocab length")
 args = parser.parse_args()
 
 OPTIMIZATION_BUDGET = args.optimization_budget
@@ -27,7 +28,9 @@ DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 print("Using device:", DEVICE)
 
 if LOG:
-    wandb.init(project=args.wandb_project, settings=wandb.Settings(start_method="thread"))
+    tags = [f"d_model={args.d_model}", f"num_layers={args.num_layers}", f"num_heads={args.num_heads}"]
+    name = f"dim_{args.d_model}-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
+    wandb.init(project=args.wandb_project, settings=wandb.Settings(start_method="thread"), tags=tags, name=name)
 
 # get dataloaders
 train_dataset, val_dataset = ArithmeticDataset.splits(
@@ -47,13 +50,13 @@ val_dataloader = ArithmeticIterator(
 )
 
 # get model: decoder-only transfrormer with causal attention masking; 2 layers, width 128, 4 attention heads
-model = Transformer(
+model = DecoderOnlyTransformer(
     n_layers=args.num_layers,
     n_heads=args.num_heads,
     d_model=args.d_model,
-)
-model.to(DEVICE)
-model.train()
+    vocab_len=args.vocab_len,
+    device=DEVICE,
+).float().to(DEVICE)
 
 # get optimizer
 optimizer = AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, betas=(args.beta1, args.beta2))
@@ -69,10 +72,8 @@ for epoch in tqdm(range(int(OPTIMIZATION_BUDGET / steps_per_epoch))):
     for i, batch in enumerate(train_dataloader):
         optimizer.zero_grad()
         X, y = batch['text'], batch['target']
-        X = X.to(DEVICE)
-        y = y.to(DEVICE)
-        y_hat, _, _ = model(X) # y_hat is (batch_size, seq_len, vocab_size)
-        # only calculate the loss on the answer part of the equation
+        X, y = X.to(DEVICE), y.to(DEVICE)
+        y_hat = model(X)
         loss = criterion(y_hat[:, -2, :], y[:, -2])
         loss.backward()
         optimizer.step()
@@ -80,26 +81,28 @@ for epoch in tqdm(range(int(OPTIMIZATION_BUDGET / steps_per_epoch))):
             wandb.log({
                 "Loss/train": loss.item(), 
                 "epoch": epoch,
-                "Accuracy/train": ((y_hat[:, -2, :]).argmax(dim=1) == y[:, -2]).float().mean().item(),
+                "Accuracy/train": (y_hat[:, -2, :].argmax(dim=1) == y[:, -2]).float().mean().item(),
             })
         else:
-            print(f"Epoch {epoch}: train loss {loss.item()}, train accuracy {((y_hat[:, -2, :]).argmax(dim=1) == y[:, -2]).float().mean().item()}")
+            print(f"Epoch {epoch}: train loss {loss.item()}, train accuracy {(y_hat[:, -2, :].argmax(dim=1) == y[:, -2]).float().mean().item()}")
     # eval
     model.eval()
     with torch.no_grad():
+        loss, accuracy = 0, 0
         for batch in val_dataloader: # only one batch
             X, y = batch['text'], batch['target']
             X, y = X.to(DEVICE), y.to(DEVICE)
-            y_hat, _, _ = model(X)
-            loss = criterion(y_hat[:, -2, :], y[:, -2])
-            if LOG:
-                wandb.log({
-                    "Loss/val": loss.item(), 
-                    "epoch": epoch,
-                    "Accuracy/val": ((y_hat[:, -2, :]).argmax(dim=1) == y[:, -2]).float().mean().item(),
-                })
-            else:
-                print(f"Epoch {epoch}: test loss {loss.item()}, test accuracy {((y_hat[:, -2, :]).argmax(dim=1) == y[:, -2]).float().mean().item()}")
+            y_hat = model(X)
+            loss += criterion(y_hat[:, -2, :], y[:, -2]).item()
+            accuracy += (y_hat[:, -2, :].argmax(dim=1) == y[:, -2]).float().mean().item()
+        if LOG:
+            wandb.log({
+                "Loss/val": loss / len(val_dataloader),
+                "epoch": epoch,
+                "Accuracy/val": accuracy / len(val_dataloader),
+            })
+        else:
+            print(f"Epoch {epoch}: test loss {loss / len(val_dataloader)}, test accuracy {accuracy / len(val_dataloader)}")
 
     # save model
     torch.save(model.state_dict(), "model.pt")
