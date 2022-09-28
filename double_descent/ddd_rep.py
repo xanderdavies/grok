58ca57cfbf9c8eff05388c751816aec6aae5cca3
Alexander Davies <xanderlaserdavies@gmail.com>
2022-09-28 18:53:57 +0100

    0 workers

--- double_descent/ddd_rep.py
+++ double_descent/ddd_rep.py
@@ -71,7 +71,7 @@ def run(perc_train):
 
     print(f"Train set includes {len(train_set.data)} images")
     BATCH_SIZE_UPDATED = min(len(train_set.data), BATCH_SIZE)
-    train_loader = DataLoader(train_set, batch_size=BATCH_SIZE_UPDATED, shuffle=True, num_workers=2)
+    train_loader = DataLoader(train_set, batch_size=BATCH_SIZE_UPDATED, shuffle=True)
     test_loader = DataLoader(test_set, batch_size=BATCH_SIZE_UPDATED, shuffle=False)
 
     """
