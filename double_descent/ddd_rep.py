a83ec64ad670dbe7a54cb6498cf5c7170628b0eb
Alexander Davies <xanderlaserdavies@gmail.com>
2022-10-05 06:13:00 +0100

    clean up

--- double_descent/ddd_rep.py
+++ double_descent/ddd_rep.py
@@ -2,35 +2,26 @@
 
 import torch
 import torch.nn as nn 
-from torch.optim import Adam, AdamW, SGD
+from torch.optim import AdamW, SGD
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, ToTensor, RandomCrop, RandomHorizontalFlip, Normalize
 
 from tqdm import tqdm
 import wandb
-from multiprocessing import Process
 from datetime import datetime
 
 from resnet_18k import make_resnet18k
 from utils import remove_train, apply_label_noise
-import argparse
 
 """
 Hyperparameters
 """
 
-# parser = argparse.ArgumentParser(description="Double Descent")
-# parser.add_argument("--dset-size", type=float, required=True)
-# arguments = parser.parse_args() 
-
-# PERC_TRAIN = arguments.dset_size
-PERC_TRAIN = [0.1, 1]
-# PERC_TRAIN = [0.001, 0.005, 0.01, 0.05, 0.1, 0.25, 0.5, 1] # [1, 0.8, 0.6, 0.4] #.001 # how much of the training data to use (1 = all)
-
+PERC_TRAIN = 1
 WIDTH_PARAM = 64 # [3, 12, 64] are in DDD
-LR =  0.0001 # 0.01 # per DDD, but fails?
-LABEL_NOISE = 2 # per DDD
+LR =  0.0001 # per DDD
+LABEL_NOISE = .2 # per DDD
 EPOCHS = 4000 # per DDD
 BATCH_SIZE = 128 # per DDD
 
@@ -38,23 +29,14 @@ WEIGHT_DECAY = 0 # per DDD, could try 1e-5
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'; print("using:", DEVICE)
 OPT = "Adam" # per DDD
 
-RANDOM_SEED = True
+RANDOM_SEED = False
 if not RANDOM_SEED:
     torch.random.manual_seed(0)
 NUM_JOBS = 1
 LOAD_PATH = None
 
-args = {
-    "Opt": OPT, 
-    "Weight Decay": WEIGHT_DECAY,
-    "Label Noise": LABEL_NOISE,
-    "Percent Training Data": PERC_TRAIN, 
-    "Learning Rate": LR, 
-    "Resnet-18 Width Parameter": WIDTH_PARAM,
-    "Random Seed": RANDOM_SEED
-}
-
-def run(perc_train):
+def run(args):
+    perc_train = args["Percent Training Data"]
     """
     Data
     """
@@ -84,23 +66,14 @@ def run(perc_train):
     if OPT == "Adam":
         optimizer = AdamW(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
     elif OPT == "SGD":
-        # optimizer = SGD(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
-        optimizer = SGD([
-            {"params": model.conv1.parameters(), "lr": LR}, 
-            {"params": model.layer1.parameters(), "lr": LR},
-            {"params": model.layer2.parameters(), "lr": LR},
-            {"params": model.layer3.parameters(), "lr": LR},
-            {"params": model.layer4.parameters(), "lr": LR},
-            {"params": model.linear.parameters(), "lr": LR},
-        ], lr=LR, weight_decay=WEIGHT_DECAY)
-
+        optimizer = SGD(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
     else:
         raise NotImplementedError()
 
     """
     Wandb
     """
-    tags = [f"perc_train_{perc_train}", OPT, f"label_noise_{LABEL_NOISE}", f"width_{WIDTH_PARAM}", f"weight_decay_{WEIGHT_DECAY}", f"LR_{LR}"]
+    tags = [f"perc_train_{perc_train}", OPT, f"label_noise_{LABEL_NOISE}", f"width_{WIDTH_PARAM}", f"weight_decay_{WEIGHT_DECAY}", f"LR_{LR}", f"SLOW_EARLY_LR_{SLOW_EARLY_LR}"]
     args["Percent Training Data"] = perc_train
     name = '-'.join(tags) + f"-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
     wandb.init(project="deep-double-descent", tags=tags, name=name, config=args)
@@ -109,6 +82,7 @@ def run(perc_train):
     """
     Train
     """
+    prev_20_accs = []
     for ep in tqdm(range(EPOCHS)):
         model.eval()
         with torch.no_grad():
@@ -119,6 +93,12 @@ def run(perc_train):
                 loss = criterion(y_pred, y)
                 total_loss += loss.item()
                 total_acc += (y_pred.argmax(dim=1) == y).float().mean().item()
+
+            
+            prev_20_accs.append((y_pred.argmax(dim=1) == y).float().mean().item())
+            if len(prev_20_accs) > 20:
+                prev_20_accs.pop(0)
+
             wandb.log({
                 "Loss/val": total_loss / len(test_loader),
                 "Accuracy/val": total_acc / len(test_loader),
@@ -133,24 +113,24 @@ def run(perc_train):
             loss = criterion(y_pred, y)
             loss.backward()
             optimizer.step()
+
             wandb.log({
                 "Loss/train": loss.item(),
                 "Accuracy/train": (y_pred.argmax(dim=1) == y).float().mean().item(),
                 "epoch": ep,
                 "Loss/normalized_train": criterion((y_pred / torch.norm(y_pred, dim=1).unsqueeze(1)), y).item()
             })
-
-        # torch.save(model.state_dict(), f"weights/{name}-LATEST-model.pt")
+            
+        torch.save(model.state_dict(), f"weights/{name}-LATEST-model.pt")
 
 if __name__ == '__main__':
-    torch.multiprocessing.set_start_method('spawn')
-    wandb.setup()
-    processes = []
-    for i in range(NUM_JOBS):
-        processes.append(Process(target=run, args=(PERC_TRAIN[i] if isinstance(PERC_TRAIN, list) else PERC_TRAIN,)))
-
-    for p in processes:
-        p.start()
-
-    for p in processes:
-        p.join()
+    args = {
+    "Opt": OPT, 
+    "Weight Decay": WEIGHT_DECAY,
+    "Label Noise": LABEL_NOISE,
+    "Percent Training Data": PERC_TRAIN, 
+    "Learning Rate": LR, 
+    "Resnet-18 Width Parameter": WIDTH_PARAM,
+    "Random Seed": RANDOM_SEED
+    }
+    run(args)
